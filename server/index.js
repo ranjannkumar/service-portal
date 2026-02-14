e84986fb5c50d5111583210add567f6729791305
ranjannkumar <ranjankumar5673@gmail.com>
2026-02-14 21:23:47 +0530

    fix: Add Docker compatibility flags for Puppeteer and start script

--- server/index.js
+++ server/index.js
@@ -32,7 +32,15 @@ const client = new Client({
         backupSyncIntervalMs: 60000
     }),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ],
         headless: true
     }
 });
